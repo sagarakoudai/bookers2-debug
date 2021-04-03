2ff5df238a25e30981851ef012aa23d1b6437792
EC2 Default User <ec2-user@ip-172-31-94-205.ec2.internal>
2021-04-03 02:59:53 +0000

    comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,20 @@
 class BookCommentsController < ApplicationController
     
     def create
-        @comment = BookComment.
-        @comment.user_id = current_user.id
-        @comment.book_id = params[:book_id]
-        # if 
+        @comment = BookComment.new(user_id: current_user.id, book_id: params[:book_id]),(comment_params)  
         @comment.save
-        redirect_back(fallback_location: root_path)
-        # else redirect_to books_path
-        # end
-        
+         redirect_back(fallback_location: root_path)
     end
     
     def destroy
-        @comment = BookComment.find_by(user_id: current_user.id, book_id: params[:id])
+        @comment = BookComment.find_by(params[:id])
         @comment.destroy
         redirect_back(fallback_location: root_path)
+    end
+    
+    private
+    def comment_params
+        params.require(:comment).permit(:user_id, :book_id, :bo)
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-    resources :book do
-    resources :book_comments,only: [:create,:destroy]
-  end
+  # resources :books do
+  #   resources :book_comments,only: [:create,:destroy]
+  # end
   
   
   
   post "likes/:id/create" => "likes#create", as: :likes
   delete "likes/:id/destroy" => "likes#destroy", as: :like
   
-  # post "/book/:id/book_comments" => "book_comments#create", as: :comments
-  # delete "book_comments/:id/destroy" => "book_comments#destroy", as: :book_book_comment
+  post "/books/:id/book_comments" => "book_comments#create", as: :comments
+  delete "/books/:id/book_comment" => "book_comments#destroy", as: :book_book_comment
  
  
   
